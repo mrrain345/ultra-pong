5e5d9a961a782d1ba41ee1211282af41dbfb4fa5
MrRaiN <mc.rain345@gmail.com>
2019-12-18 00:23:50 +0100

    Rackets and Ball

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour {
+  
+  public float speed = 10f;
+  public float hitAcceleration = 0.1f;
+
+  new Rigidbody2D rigidbody;
+  int hits = 0;
+
+  void Start() {
+    rigidbody = GetComponent<Rigidbody2D>();
+    rigidbody.velocity = Vector2.left * speed;
+  }
+  
+  float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight) {
+    return (ballPos.y - racketPos.y) / racketHeight;
+  }
+
+  void OnCollisionEnter2D(Collision2D col) {
+    if (col.gameObject.CompareTag("Racket")) {
+      float y = hitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
+      Vector2 dir = new Vector2(col.transform.right.x, y).normalized;
+      rigidbody.velocity = dir * (speed + hits * hitAcceleration);
+      hits++;
+    }
+  }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+  public float speed = 7;
+  public string axis = "Vertical";
+
+  new Rigidbody2D rigidbody;
+
+  void Start() {
+    rigidbody = GetComponent<Rigidbody2D>();
+  }
+
+  void FixedUpdate () {
+    float vel = Input.GetAxisRaw(axis) * speed * Time.fixedDeltaTime;
+    rigidbody.velocity = new Vector2(0, vel);
+  }
+}
