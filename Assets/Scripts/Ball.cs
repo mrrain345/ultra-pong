2381277ea5c16f861ee7c584d334289f6dde7ad5
MrRaiN <mc.rain345@gmail.com>
2019-12-18 03:45:50 +0100

    Ball explosion

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,6 +4,7 @@ public class Ball : MonoBehaviour {
   
   public float speed = 10f;
   public float hitAcceleration = 0.1f;
+  public GameObject explodeObj;
 
   new Rigidbody2D rigidbody;
   int hits = 0;
@@ -18,11 +19,17 @@ public class Ball : MonoBehaviour {
   }
 
   void OnCollisionEnter2D(Collision2D col) {
-    if (col.gameObject.CompareTag("Racket")) {
-      float y = hitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
-      Vector2 dir = new Vector2(col.transform.right.x, y).normalized;
-      rigidbody.velocity = dir * (speed + hits * hitAcceleration);
-      hits++;
-    }
+    if (!col.gameObject.CompareTag("Racket")) return;
+    float y = hitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
+    Vector2 dir = new Vector2(col.transform.right.x, y).normalized;
+    rigidbody.velocity = dir * (speed + hits * hitAcceleration);
+    hits++;
+  }
+
+  void OnTriggerEnter2D(Collider2D col) {
+    if (!col.gameObject.CompareTag("Finish")) return;
+    GameObject obj = GameObject.Instantiate(explodeObj, transform.position + Vector3.back, transform.rotation);
+    obj.GetComponent<Explosion>().Explode();
+    Destroy(gameObject);
   }
 }
---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+  
+  public float multiplier = 5f;
+  public float lifeTime = 3f;
+
+  public void Explode(Vector3 velocity = new Vector3()) {
+    foreach (Transform child in transform) {
+      Rigidbody2D rbody = child.GetComponent<Rigidbody2D>();
+      rbody.constraints = RigidbodyConstraints2D.None;
+      rbody.velocity = child.localPosition * multiplier + velocity;
+    }
+
+    Destroy(gameObject, lifeTime);
+  }
+}
